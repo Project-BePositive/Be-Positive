d71c9bcf0c8c5f224f13e62b2864c27d39d08e9f
Project-BePositive <103304670+Project-BePositive@users.noreply.github.com>
2022-04-16 15:00:21 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './Components/Navbar/NavBar.jsx';
+import DonorForm from './Components/Form/DonorForm/form';
+import DonorList from './Components/List/Donor/donor';
+import Login from './Components/List/Donor/login';
+import UpdateDonor from './Components/List/Donor/update';
 
+const App = () =>{
+    return(
+     <Router>
+         <NavBar />
+        <Route exact path="/bepositive/donateblood" component={DonorForm} />
+        <Route exact path="/bepositive/getdonors" component={DonorList}/>
+        <Route exact path="/bepositive/login" component={Login}/>
+        <Route exact path="/bepositive/loginanddelete" component={Login}/>
+        <Route exact path="/bepositive/updatedonor" component={UpdateDonor}/>
+    </Router>
+    )
+}
+
+export default App;
