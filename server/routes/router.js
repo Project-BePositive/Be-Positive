a308bb3499b8abac2eb8ad66bbcd73c877f035f5
Project-BePositive <103304670+Project-BePositive@users.noreply.github.com>
2022-11-26 18:40:54 +0530

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,27 +1,26 @@
 const express=require("express");
 const router=express.Router();
 const donors=require("../models/donorSchema");
-
 //user registration
 router.post("/register",async(req,res)=>{
     //validating the form
     //console.log(req.body);
-    const {name,email,password,gender,age,weight,bloodgroup,phno,state,city} = req.body;
+    const {name,email,aadhar,password,gender,age,weight,bloodgroup,phno,state,city,disease} = req.body;
 
     //if users miss any fields
-    if(!name || !email || !password || !gender || !age || !weight || !bloodgroup || !phno || !state || !city){
+    if(!name || !email || !aadhar || !password || !gender || !age || !weight || !bloodgroup || !phno || !state || !city || !disease){
         return res.status(404).json("Please fill all the fields");
     }
 
     try{
-        const preuser = await donors.findOne({email:email}) //checking the email with users db emails
+        const preuser = await donors.findOne({aadhar:aadhar}) //checking the email with users db emails
         console.log(preuser); //if user not present it shows null
 
         if(preuser){
-            res.status(404).json("Already registered");
+            return res.status(404).json("Already registered");
         }else{
             const adduser = new donors({
-                name,email,password,gender,age,weight,bloodgroup,phno,state,city
+                name,email,aadhar,password,gender,age,weight,bloodgroup,phno,state,city,disease
             });
 
             await adduser.save();
@@ -34,7 +33,7 @@ router.post("/register",async(req,res)=>{
     }
 })
 
-//get userdata
+//get userdata for list page
 router.get("/getdata",async(req,res)=>{
     try{
         const userdata=await donors.find();
@@ -44,7 +43,7 @@ router.get("/getdata",async(req,res)=>{
         res.status(404).json(error);
     }
 })
-module.exports=router;
+
 
 //delete userdata
 router.delete("/deleteuser/:id",async(req,res)=>{
@@ -58,7 +57,7 @@ router.delete("/deleteuser/:id",async(req,res)=>{
     }
 })
 
-//find a particular user
+//find a particular user for update 
 router.get("/getuser/:id",async(req,res)=>{
     try{
         console.log(req.params);
@@ -85,7 +84,7 @@ router.patch("/updateuser/:id",async(req,res)=>{
     }
 })
 
-//check user data
+//check user data for login page
 router.post('/checkdonor', async(req,res)=>{
     const email=req.body.email;
     const password=req.body.password;
@@ -114,3 +113,47 @@ router.post('/checkdonor', async(req,res)=>{
         return res.status(422).json({error:"Error!"});
     }
 })
+
+router.post("/sendmail/:id",async(req,res)=>{
+    //console.log(req.body);
+    var nodemailer=require('nodemailer');
+    var msg=`Hey,Iam ${req.body.name}! Iam in urgent need of plasma due to ${req.body.purpose}.
+    \nIf you are available to donate your plasma,kindly contact me on this number ${req.body.contact}.
+    \nThankyou in advance!`;
+    
+    try{
+        const {id} = req.params;
+        const user= await donors.findById({_id:id});
+        console.log(user);
+        var transporter=nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user:'bepositiveplasmadonation@gmail.com',
+                pass:'llmmdslsuemcxfos',
+            }
+        });
+        
+        var mailOptions={
+            from:'bepositiveplasmadonation@gmail.com',
+            to: user.email,
+            subject:'Test',
+            text: msg
+        };
+        
+        transporter.sendMail(mailOptions,function(error,info){
+            if(error){
+                console.log(error);
+                transporter.close();
+                return res.status(422).json({error:"Error!"});
+            }else{
+                transporter.close();
+                console.log('Email sent: '+info.response);
+                return res.status(200).json({id:emailExist._id});
+            }
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+})
+module.exports=router;
